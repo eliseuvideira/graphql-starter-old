b926140f0b5627bf18c50eff6955a0bbb0f72736
Eliseu Videira <eliseuvideira1@gmail.com>
2021-04-25 23:26:12 -0300

    :sparkles: added errors middlewares

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
 import express from "express";
 import cors from "cors";
+import { exception, notFound } from "@ev-fns/errors";
 
 const app = express();
 
 app.use(cors());
 
 app.get("/", (req, res) => res.redirect("/graphql"));
+
+app.use(notFound);
+app.use(exception);
 
 export default app;
