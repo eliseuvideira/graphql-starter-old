32aa2a37cb365ee8a8fd2a8050ddf98d4dcb5e72
Eliseu Videira <eliseuvideira1@gmail.com>
2021-04-25 23:22:10 -0300

    :sparkles: added dotenv

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,10 @@
+import dotenv from "@ev-fns/dotenv";
+
+dotenv({}, ({ NODE_ENV, npm_package_version }) => {
+  console.info(NODE_ENV);
+  console.info(npm_package_version);
+});
+
 import http from "http";
 import app from "./app";
 import { apollo } from "./apollo";
@@ -17,7 +24,7 @@ import { apollo } from "./apollo";
     server.listen(port);
   });
 
-  console.info(`listening at http://localhost:${port}`);
+  console.info(`http://localhost:${port}`);
 })().catch((err) => {
   console.error(err);
   process.exit(1);
